71f9a1ac312d5ce51d93ca280da9469d5c1f979d
Sami Jawhar <sami.n.jawhar@gmail.com>
2016-06-23 06:39:49 -0700

    Initial commit

---
+++ foods.js
@@ -0,0 +1,54 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+module.exports = {
+    "sugar": "There are many low carb sweeteners, but my favorites are erythritol and stevia because they don't affect your blood sugar. Use one cup erythritol together with one quarter teaspoon stevia to replace one cup of sugar.",
+    "sweeteners": "There are many low carb sweeteners, but my favorites are erythritol and stevia because they don't affect your blood sugar. Use one cup erythritol together with one quarter teaspoon stevia to replace one cup of sugar.",
+    "sweetener": "There are many low carb sweeteners, but my favorites are erythritol and stevia because they don't affect your blood sugar. Use one cup erythritol together with one quarter teaspoon stevia to replace one cup of sugar.",
+    "flour": "Coconut flour, almond flour, and flaxseed meal can replace wheat flour in many recipes. Which one you choose depends on the recipe. For that wheat-like chewiness, try adding psyllium husk powder.",
+    "chocolate": "You can use unsweetened baking chocolate or unsweetened cocoa, together with low carb sweeteners, to add chocolate flavor to recipes. To make keto chocolate, melt and combine unsweetened baking chocolate, butter, heavy cream, vanilla, and sweetener.",
+    "rice": "Use the grating disk of a food processor to process cauliflower into rice-sized granules, then cook in a pan. You can also buy sheerataki rice, also known as miracle rice, which is made from the coneyak yam.",
+    "potatoes": "For mashed potatoes, use mashed cauliflower. For fries and chips, try celeriac.",
+    "potato": "For mashed potatoes, use mashed cauliflower. For fries and chips, try celeriac.",
+    "bread crumbs": "Use a food processor to grind pork rinds into crumbs. Depending on the recipe, combine pork rind crumbs with grated parmesan cheese.",
+    "yogurt": "Full fat greek yogurt and full fat cottage cheese are OK in moderation. You can also make your own coconut milk yogurt.",
+    "oatmeal": "Chia seed pudding is one of my favorite hot breakfasts. You can also make an oatmeal-like hot breakfast from coconut flour and psyllium husk.",
+    "tortillas": "You can easily make tortillas from flaxseed meal, psyllium husk powder, olive oil, and water. You can also buy low-carb high fiber tortillas from the grocery store.",
+    "tortilla": "You can easily make tortillas from flaxseed meal, psyllium husk powder, olive oil, and water. You can also buy low-carb high fiber tortillas from the grocery store.",
+    "pasta": "Try sheerataki noodles for your next pasta dish. You can also make noodles from vegetables like zucchini.",
+    "noodles": "You can make noodles from vegetables like zucchini. You can also buy sheerataki noodles from the store. Just make sure to rinse and boil them.",
+    "noodle": "You can make noodles from vegetables like zucchini. You can also buy sheerataki noodles from the store. Just make sure to rinse and boil them.",
+    "chips": "Try snacking on some pork rinds. They're delicious. You can make chips from cheese, flaxseed meal, or celeriac. For dips, you can also use celery.",
+    "chip": "Try snacking on some pork rinds. They're delicious. You can make chips from cheese, flaxseed meal, or celeriac. For dips, you can also use celery.",
+    "crackers": "Try snacking on some pork rinds. They're delicious. You can make crackers from almond flour, flaxseed meal, or cheese. For dips, you can also use celery.",
+    "cracker": "Try snacking on some pork rinds. They're delicious. You can make crackers from almond flour, flaxseed meal, or cheese. For dips, you can also use celery.",
+    "fruit": "If you need fruit flavors in a recipe, try using good quality extracts. For easy, on-the-go snacks, try cheese and nuts.",
+    "frosting": "You can make keto-safe frosting from cream cheese or butter and low carb sweeteners.",
+    "pudding": "You can make pudding from cream cheese, whipped cream, chia seeds, or even avocados.",
+    "cake": "Almond flour can be used to make a pretty delicious low carb cake.",
+    "beer": "Dry wines or hard liquor, in moderation, can help you stay keto while out on the town.",
+    "candy": "There are no shortage of keto-friendly sweets out there. Fat bombs and mug cakes are some of the most common. For something quick and easy, try snacking on some cheese and nuts.",
+    "cookies": "Almond flour or coconut flour make for a great keto-friendly cookie recipe.",
+    "cookie": "Almond flour or coconut flour make for a great keto-friendly cookie recipe.",
+    "fried chicken": "Coat your chicken in pork rind crumbs and parmesan cheese for a keto-friendly crisp.",
+    "pancakes": "You can make pancakes or waffles out of almond flour, coconut flour, cream cheese, protein powder, even peanut butter and flaxseed meal. Take your pick!",
+    "pancake": "You can make pancakes or waffles out of almond flour, coconut flour, cream cheese, protein powder, even peanut butter and flaxseed meal. Take your pick!",
+    "waffles": "You can make pancakes or waffles out of almond flour, coconut flour, cream cheese, protein powder, even peanut butter and flaxseed meal. Take your pick!",
+    "waffle": "You can make pancakes or waffles out of almond flour, coconut flour, cream cheese, protein powder, even peanut butter and flaxseed meal. Take your pick!",
+    "bagels": "Have you tried making bagels from mozarella, cream cheese, and almond flour? They're delicious.",
+    "bagel": "Have you tried making bagels from mozarella, cream cheese, and almond flour? They're delicious.",
+    "hamburger buns": "Try wrapping your burger in lettuce, or forget the bun and eat with a knife and fork. You can make a low carb hamburger bun in the microwave using egg, almond flour, and psullium husk.",
+    "hamburger bun": "Try wrapping your burger in lettuce, or forget the bun and eat with a knife and fork. You can make a low carb hamburger bun in the microwave using egg, almond flour, and psullium husk.",
+    "protein bars": "Quest protein bars are sweetened with erythritol and can be keto-friendly in a pinch. You can make protein bars from nuts, almond butter, and flaxseed meal.",
+    "protein bar": "Quest protein bars are sweetened with erythritol and can be keto-friendly in a pinch. You can make protein bars from nuts, almond butter, and flaxseed meal.",
+    "granola": "Try mixing a variety of chopped nuts with unsweetened coconut flakes for an easy trail snack.",
+    "cereal": "Toasted and chopped nuts with unsweetened coconut flakes and almond milk make an easy keto breakfast. If you're feeling adventurous, Google salted caramel pork rind cereal. You can thank me later.",
+    "pizza": "You can make pizza crust from cauliflower, cheese and almond meal, even ground chicken and cheese. Also, make sure your tomato sauce doesn't have any added sugar."
+};
---
+++ index.js
@@ -0,0 +1,116 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+/**
+ * This sample shows how to create a Lambda function for handling Alexa Skill requests that:
+ *
+ * - Custom slot type: demonstrates using custom slot types to handle a finite set of known values
+ *
+ * Examples:
+ * One-shot model:
+ *  User: "Alexa, ask Keto Pal what can I use instead of sugar."
+ *  Alexa: "(reads back keto-friendly substitution for sugar)"
+ */
+
+'use strict';
+
+var AlexaSkill = require('./AlexaSkill'),
+    foods = require('./foods');
+
+var APP_ID = 'amzn1.echo-sdk-ams.app.d8a64ff8-0913-4ff9-a01b-743e8741eb8a';
+
+/**
+ * HowTo is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var HowTo = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+HowTo.prototype = Object.create(AlexaSkill.prototype);
+HowTo.prototype.constructor = HowTo;
+
+HowTo.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    var speechText = "Welcome to Keto Pal. You can ask a question like, what's a substitution for flour? Or, how can I make a low carb pizza? ... Now, how can I help you stay keto?";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    var repromptText = "For instructions on what you can say, please say help me.";
+    response.ask(speechText, repromptText);
+};
+
+HowTo.prototype.intentHandlers = {
+    "SubstitutionIntent": function (intent, session, response) {
+        var foodSlot = intent.slots.Food,
+            foodName;
+        if (foodSlot && foodSlot.value){
+            foodName = foodSlot.value.toLowerCase();
+        }
+
+        var cardTitle = "Keto friendly " + foodName,
+            substitution = foods[foodName],
+            speechOutput,
+            repromptOutput;
+        if (substitution) {
+            speechOutput = {
+                speech: substitution,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.tellWithCard(speechOutput, cardTitle, substitution);
+        } else {
+            var speech;
+            if (foodName) {
+                speech = "I'm sorry, I currently do not know how to make " + foodName + " keto. What else can I help with?";
+            } else {
+                speech = "I'm sorry, I currently do not know that food. What else can I help with?";
+            }
+            speechOutput = {
+                speech: speech,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            repromptOutput = {
+                speech: "What else can I help with?",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.ask(speechOutput, repromptOutput);
+        }
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Thanks for using Keto Pal. Keep calm and keto on.";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Thanks for using Keto Pal. Keep calm and keto on.";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        var speechText = "You can ask me questions like, what can I use for sugar? Or, how can I have bagels on keto? Or, you can say exit... Now, how can I help you stay keto?";
+        var repromptText = "You can ask me questions like, what's a low carb sweetener? Or, how can I make keto bagels? Or, you can say exit... Now, how can I help you stay keto?";
+        var speechOutput = {
+            speech: speechText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        var repromptOutput = {
+            speech: repromptText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    }
+};
+
+exports.handler = function (event, context) {
+    var howTo = new HowTo();
+    howTo.execute(event, context);
+};
